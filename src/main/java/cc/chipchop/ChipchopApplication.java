5bb73e4266c89ca377a80cecdf0ce4c344d4fac1
Natia Natisvili <natianatisvili@GLaDOS.local>
2025-02-25 10:20:08 +0200

    initial commit - project setup

---
+++ src/main/java/cc/chipchop/ChipchopApplication.java
@@ -0,0 +1,13 @@
+package cc.chipchop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChipchopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChipchopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cc/chipchop/ChipchopApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.chipchop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChipchopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
