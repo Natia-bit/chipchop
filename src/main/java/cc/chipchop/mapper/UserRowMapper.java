c96cd8e707151dc8f8e16e53af526419404d4d30
Natia Natisvili <natianatisvili@GLaDOS.local>
2025-03-05 13:40:50 +0200

    user rowMapper added

---
+++ src/main/java/cc/chipchop/mapper/UserRowMapper.java
@@ -0,0 +1,20 @@
+package cc.chipchop.mapper;
+
+import cc.chipchop.entity.User;
+
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserRowMapper implements RowMapper<User> {
+
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return  new User(
+                rs.getLong(1),
+                rs.getString(2),
+                rs.getString(3)
+        );
+    }
+
+}
