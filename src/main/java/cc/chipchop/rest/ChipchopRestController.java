1c852de906f2f364b6ff0698584fea91f60327d2
Natia Natisvili <natianatisvili@GLaDOS.local>
2025-03-07 13:26:41 +0200

    added postgres drivers and schema

---
+++ src/main/java/cc/chipchop/rest/ChipchopRestController.java
@@ -0,0 +1,4 @@
+package cc.chipchop.rest;
+
+public class ChipchopRestController {
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+id SERIAL PRIMARY KEY,
+email TEXT UNIQUE NOT NULL,
+password TEXT NOT NULL
+);
+
+insert INTO "user"(email, password)
+values
+('one@example.com', 'secretpass'),
+('two.example.com', 'verysecretpass')
+('three@example.com', 'supersecretpass')
