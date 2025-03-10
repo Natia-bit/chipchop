dc9213366db7246c8090c2a0230e1fa57bcc03e0
Natia Natisvili <natianatisvili@GLaDOS.local>
2025-03-10 09:39:00 +0200

    test userDao findAll

--- src/test/java/cc/chipchop/services/UserServiceTest.java
+++ src/test/java/cc/chipchop/services/UserServiceTest.java
@@ -12,9 +12,13 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.Mockito.when;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -29,9 +33,9 @@ public class UserServiceTest {
     public void serUp()
     {
         when(userDao.findById(1)).thenReturn(Optional.empty());
-        when(userDao.findById(2)).thenReturn(Optional.of(new User(2, "bob@example.com", "secretpass")));
-        when(userDao.findById(3)).thenReturn(Optional.of(new User(3, "alice@example.com", "supersecretpass")));
-        when(userDao.findById(4)).thenReturn(Optional.of(new User(4, "alex@example.com", "topsecretpass")));
+        when(userDao.findById(2)).thenReturn(Optional.of(new User(2, "bob@test.com", "secretpass")));
+        when(userDao.findById(3)).thenReturn(Optional.of(new User(3, "alice@test.com", "supersecretpass")));
+        when(userDao.findById(4)).thenReturn(Optional.of(new User(4, "alex@test.com", "topsecretpass")));
     }
     @AfterEach
     public void tearDown(){
@@ -40,7 +44,17 @@ public class UserServiceTest {
 
     @Test
     public void givenFindAll_whenDaoReturnsMultipleRecords_ThenReturnMultipleRecords(){
+        List<User> expected = new ArrayList<>();
+        expected.add(new User(1, "tim@test.com", "123456"));
+        expected.add(new User(2, "rob@test.com", "102030"));
+        expected.add(new User(3, "maria@test.com", "654321"));
 
+        when(userDao.findAll()).thenReturn(expected);
+
+        var actual = userService.findAll();
+
+        assertEquals(expected, actual);
+        verify(userDao, times(1)).findAll();
     }
 
     @Test
@@ -63,6 +77,11 @@ public class UserServiceTest {
 
     @Test
     public void givenUpdate_whenUpdatingUser_thenReturnUpdatedUser(){
+
+    }
+
+    @Test
+    public void givenDelete_whenDeletingExistingUser_thenReturnTrue(){
 
     }
 }
