00847cafcc31c5f95c086c17dc625fcdd2579837
Natia Natisvili <natianatisvili@GLaDOS.local>
2025-06-06 12:52:34 +0200

    sessionManagment added

--- src/main/java/cc/chipchop/config/SecurityConfig.java
+++ src/main/java/cc/chipchop/config/SecurityConfig.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,19 +22,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests(authorize -> authorize
-            .requestMatchers("/public/**" ).permitAll()
-            .anyRequest().authenticated()
-            )
-            .formLogin(form -> form
-            .loginPage("/login")
-            .permitAll()
-            )
-            .logout(LogoutConfigurer::permitAll)
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .httpBasic(withDefaults -> {})
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().authenticated())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .userDetailsService(userDetailService);
-
 
         return http.build();
     }
-
 }
--- src/main/java/cc/chipchop/service/UserDetailServiceImpl.java
+++ src/main/java/cc/chipchop/service/UserDetailServiceImpl.java
@@ -4,7 +4,9 @@ import cc.chipchop.entity.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailServiceImpl implements UserDetailsService {
 
     private final UserService userService;
@@ -13,18 +15,13 @@ public class UserDetailServiceImpl implements UserDetailsService {
         this.userService = userService;
     }
 
-
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userService.findByEmail(email);
 
-        if (user == null) {
-            throw new UsernameNotFoundException("Not found");
-        }
-
         return org.springframework.security.core.userdetails.User.builder()
             .username(user.email())
-            .password(user.password())
+            .password("{noop}" + user.password())
             .build();
     }
 }
