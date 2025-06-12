5508a6434bb112385999f21ef530a98f75eb3d64
Natia Natisvili <natianatisvili@GLaDOS.local>
2025-06-12 09:55:20 +0200

    test configuration with @SpringBoot and @AutoConfigureMock

--- src/test/java/cc/chipchop/config/SecurityConfigTest.java
+++ src/test/java/cc/chipchop/config/SecurityConfigTest.java
@@ -1,23 +1,15 @@
 package cc.chipchop.config;
 
 import cc.chipchop.entity.User;
-import cc.chipchop.rest.ChipchopRestController;
-import cc.chipchop.service.UserDetailServiceImpl;
 import cc.chipchop.service.UserService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -26,7 +18,6 @@ import java.util.Optional;
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -42,11 +33,6 @@ public class SecurityConfigTest {
 
     @MockBean
     private UserService userService;
-
-    @BeforeEach
-    void setup() {
-
-    }
 
     private final User mockUser = new User(1, "test@example.com", "password");
 
