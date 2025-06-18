15ec276e2acc8a770c075563c7df9ec539b925c7
Natia Natisvili <natianatisvili@GLaDOS.local>
2025-06-18 10:17:00 +0200

    edit schema to have roles

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,3 +9,15 @@ values
     ('one@example.com', 'secretpass'),
     ('two.example.com', 'verysecretpass'),
     ('three@example.com', 'supersecretpass');
+
+
+CREATE TABLE IF NOT EXISTS roles (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+insert INTO roles (name)
+values
+    ('user'),
+    ('admin'),
+
