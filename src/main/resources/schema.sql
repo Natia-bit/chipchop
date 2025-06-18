4a6f9d93ce61a7f6f2cbf7d8d2717ef6199de01c
Natia Natisvili <natianatisvili@GLaDOS.local>
2025-06-18 15:20:51 +0200

    schema for user_roles

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,11 +13,20 @@ values
 
 CREATE TABLE IF NOT EXISTS roles (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL
+    name TEXT NOT NULL UNIQUE
 );
 
 insert INTO roles (name)
 values
     ('user'),
     ('admin'),
+
+
+CREATE TABLE IF NOT EXISTS user_roles (
+    user_id BIGINT NOT NULL,
+    role_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (roles_id) REFERENCES role(id) ON DELETE CASCADE
+)
 
