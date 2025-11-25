090c1c205a4e420497b743c7775d1651892620b8
Natia Natisvili <natianatisvili@GLaDOS.local>
2025-11-25 11:28:45 +0200

    added at UserRole insert and find by id

--- src/main/java/cc/chipchop/dao/UserRoleDao.java
+++ src/main/java/cc/chipchop/dao/UserRoleDao.java
@@ -3,11 +3,13 @@ package cc.chipchop.dao;
 import cc.chipchop.entity.UserRole;
 import cc.chipchop.mapper.UserRoleRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class UserRoleDao {
@@ -22,5 +24,20 @@ public class UserRoleDao {
     public List<UserRole> findAll(){
         var query = "SELECT user_id, role FROM user_roles";
         return this.jdbcTemplate.query(query, this.rowMapper);
+    }
+
+    public int insert(UserRole userRole) {
+        var query = "INSERT INTO user_roles(user_id, role) VALUES (?, ?) ";
+            return this.jdbcTemplate.update(query, userRole.userId(), userRole.role());
+    }
+
+    public Optional<UserRole> findRoleByUserId( long userId){
+        var query = "SELECT user_id FROM user_roles WHERE user_id = ?";
+        try {
+            return Optional.ofNullable(this.jdbcTemplate.queryForObject(query, this.rowMapper, userId));
+        } catch (EmptyResultDataAccessException e){
+            return Optional.empty();
+        }
+
     }
 }
