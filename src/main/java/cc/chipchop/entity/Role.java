2dc33aa4246e8ce82a2b8ac10c31f22adcc9616e
Natia Natisvili <natianatisvili@GLaDOS.local>
2025-11-25 14:11:20 +0200

    handle text case when adding roles

--- src/main/java/cc/chipchop/entity/Role.java
+++ src/main/java/cc/chipchop/entity/Role.java
@@ -1,6 +1,20 @@
 package cc.chipchop.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Role {
     USER,
     ADMIN;
+
+//    @JsonCreator
+//    public static Role fromString(String value) {
+//        return Role.valueOf(value.toUpperCase());
+//    }
+//
+//    @JsonValue
+//    public String toValue() {
+//        return this.name();
+//    }
 }
